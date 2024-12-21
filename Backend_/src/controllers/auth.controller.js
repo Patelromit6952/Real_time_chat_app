2c0c0b39576715b7c726b4e619dbd3db65519327
romitpatel <patelromit6952@gmail.com>
2024-12-21 21:52:39 +0530

    hi

--- Backend_/src/controllers/auth.controller.js
+++ Backend_/src/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const signup = async (req, res) => {
     if (password.length < 6) {
       return res.status(400).json({ message: "Password must be at least 6 characters" });
     }
-    const user = await User.findOne({email}).select('_id email');;
+    const user = await User.findOne({email});
     if (user) return res.status(400).json({ message: "Email already exists" });
 
     const salt = await bcrypt.genSalt(10);
@@ -24,20 +24,18 @@ export const signup = async (req, res) => {
       email,
       password: hashedPassword,
     });
-
     await newUser.save();
-    if (newUser) {
+    // if (newUser) {
       generateToken(newUser._id, res);
-
       res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
         email: newUser.email,
         profilePic: newUser.profilePic,
       });
-    } else {
+    // } else {
       res.status(400).json({ message: "Invalid user data" });
-    }
+    // }
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
--- Backend_/src/lib/db.js
+++ Backend_/src/lib/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {  
-
-      serverSelectionTimeoutMS: 1200000 // Increase timeout to 30 seconds
-    });
-    console.log(`MongoDB connected:`);
+    const conn = await mongoose.connect(process.env.MONGO_URI); 
   } catch (error) {
     console.log("MongoDB connection error:", error);
   }
