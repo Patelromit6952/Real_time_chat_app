037c2643d70c0f0ca8ecac9107d008ce90d77e96
romitpatel <patelromit6952@gmail.com>
2024-12-21 17:28:31 +0530

    chat app

--- Backend_/src/lib/db.js
+++ Backend_/src/lib/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {  
       useUnifiedTopology: true, 
-      serverSelectionTimeoutMS: 60000 // Increase timeout to 30 seconds
+      serverSelectionTimeoutMS: 1200000 // Increase timeout to 30 seconds
     });
     console.log(`MongoDB connected:`);
   } catch (error) {
