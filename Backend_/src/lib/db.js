eb73389a7e71b3318121b34631711ac9957a255c
romitpatel <patelromit6952@gmail.com>
2024-12-21 17:20:01 +0530

    chat app

--- Backend_/src/lib/db.js
+++ Backend_/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = awaitmongoose.connect(process.env.MONGO_URI, { 
+    const conn = await mongoose.connect(process.env.MONGO_URI, { 
       useNewUrlParser: true, 
       useUnifiedTopology: true, 
       serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
