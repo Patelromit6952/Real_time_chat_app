9ac14eb90894f8c3b39abc23578fbb6be78c1237
romitpatel <patelromit6952@gmail.com>
2024-12-22 11:36:35 +0530

    complete

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "production" ? "http://localhost:3000" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -16,9 +16,7 @@ export const useAuthStore = create((set, get) => ({
 
   checkAuth: async () => {
     try {
-      const res = await axiosInstance.get("/auth/check");
-      console.log("romit");
-      
+      const res = await axiosInstance.get("/auth/check");      
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
