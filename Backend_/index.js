f6cf91ec4f3e80d69811871edf9b6785def80aae
romitpatel <patelromit6952@gmail.com>
2024-12-23 17:23:14 +0530

    complete

--- Backend_/index.js
+++ Backend_/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://chat-app-ssr8.onrender.com"],
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
